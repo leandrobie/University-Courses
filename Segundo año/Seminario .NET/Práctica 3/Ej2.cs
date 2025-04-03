faa8d52c7114212a2c80f78cced14afdeeb79551
LeanBie <lean.bie@hotmail.com>
2025-04-03 19:47:22 -0300

    2025

---
+++ Segundo año/Seminario .NET/Práctica 3/Ej2.cs
@@ -0,0 +1,24 @@
+﻿/*Implementar un método para imprimir por consola todos los elementos de una matriz (arreglo de dos
+dimensiones) pasada como parámetro. Debe imprimir todos los elementos de una fila en la misma línea
+en la consola.*/
+
+/*creación y carga de la matriz*/
+int [,] mat= new int[3,3];
+for (int i=0;i<=8;i++)
+{
+    mat[i/3,i%3] = i;
+}
+
+ImprimirMatriz(mat);
+
+void ImprimirMatriz(int[,] matriz)
+{
+    for (int i=0;i<matriz.GetLength(0);i++)
+    {
+        for (int j=0;j<matriz.GetLength(1);j++)
+        {
+            Console.Write(matriz[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
