6eda5c470ae7dff7574dab59a2ef61ae5ad97674
LeanBie <lean.bie@hotmail.com>
2025-04-25 21:39:09 -0300

    UNLP Informatica 2025

---
+++ Segundo año/Seminario .NET/Práctica 6/Ejercicio 3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*¿Por qué no funciona el siguiente código? ¿Cómo se puede solucionar fácilmente?
+
+class Auto
+{
+double velocidad;
+public virtual void Acelerar() => Console.WriteLine("Velocidad = {0}", velocidad += 10);
+}
+class Taxi : Auto
+{
+public override void Acelerar() => Console.WriteLine("Velocidad = {0}", velocidad += 5);
+
+RTA: el campo velocidad de la clase Auto es privado, por lo tanto solo puede ser accedida desde la misma clase. La solución puede ser
+transformarla en una propiedad pública o declararla protected.
+*/
+
---
+++ Segundo año/Seminario .NET/Práctica 6/Ejercicio 4/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Contestar sobre el siguiente programa:
+Taxi t = new Taxi(3);
+Console.WriteLine($"Un {t.Marca} con {t.Pasajeros} pasajeros");
+class Auto
+{
+public string Marca { get; private set; } = "Ford";
+public Auto(string marca) => this.Marca = marca;
+public Auto() { }
+}
+class Taxi : Auto
+{
+public int Pasajeros { get; private set; }
+public Taxi(int pasajeros) => this.Pasajeros = pasajeros;
+}
+¿Por qué no es necesario agregar :base en el constructor de Taxi? Eliminar el segundo constructor de
+la clase Auto y modificar la clase Taxi para el programa siga funcionando.
+
+RTA: No es necesario agregar :base en el constructor de Taxi debido a que, en la clase Auto, se cuenta con un constructor 
+"por defecto" (es decir, que no recibe parámetros). Lo que permite a la clase Taxi no tener que llamar al constructor de
+la superclase de manera explícita (con el :base).
+Al eliminar el segundo constructor de la clase Auto, hay que modificar el constructor de la clase Taxi añadiéndole :base.
+*/
+
