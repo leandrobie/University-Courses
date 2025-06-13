97406f980473b119f45dd75eba300af3aa9cdfc8
Leandro Bie <107581384+leandrobie@users.noreply.github.com>
2025-06-13 18:12:39 -0300

    UNLP Informatica 2025

---
+++ Segundo año/Seminario .NET/Práctica 8/Ejercicio 3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+IEnumerable Rango(int pri, int ult, int paso)
+{
+    for (; pri <= ult; pri += paso)
+        yield return pri;
+}
+
+IEnumerable Potencias(int num, int potencia)
+{
+    for (int i = 1; i <= potencia; i++)
+        yield return Math.Pow(num, i);
+}
+
+IEnumerable DivisiblesPor(IEnumerable rango, int divisible)
+{
+    foreach (int i in rango)
+    {
+        if (i % divisible == 0) yield return i;
+    }
+}
+
+IEnumerable rango = Rango(6, 30, 3);
+IEnumerable potencias = Potencias(2, 10);
+IEnumerable divisibles = DivisiblesPor(rango, 6);
+
+foreach (int i in rango)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+foreach (double i in potencias)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+foreach (int i in divisibles)
+{
+Console.Write(i + " ");
+}
+Console.WriteLine();
